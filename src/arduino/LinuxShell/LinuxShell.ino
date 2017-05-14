d0e4b4f523210c0d1080e29460ab288531fcf671
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-05-14 18:24:34 -0300

    updated arduino code

---
+++ src/arduino/LinuxShell/LinuxShell.ino
@@ -0,0 +1,30 @@
+
+/* this sketch is used to connect to the Galileo's linux shell
+ * to go back to arduino mode, use the following commands in the shell:
+ *
+ * rm /sketch/sketch.elf
+ * cp /etc/inittab.bak /etc/inittab
+ * kill -SIGHUP 1
+ * 
+ * tutorial: https://learn.sparkfun.com/tutorials/galileo-getting-started-guide/using-the-terminal
+ */
+
+void setup() 
+{
+  system("cp /etc/inittab /etc/inittab.bak");  // Back up inittab
+  // Replace all "S:2345" with "S0:2345"'s (switching serial ports):
+  system("sed -i 's/S:2345/S0:2345/g' /etc/inittab");
+  // Replace all "ttyS1" with "ttyGS0"'s (switching serial ports):
+  system("sed -i 's/ttyS1/ttyGS0/g' /etc/inittab");
+  // Replace all "grst" with "#grst"'s to comment that line out:
+  system("sed -i 's/grst/#grst/g' /etc/inittab");
+  // Replace all "clld" with "#clld"'s to comment that line out:
+  system("sed -i 's/clld/#clld/g' /etc/inittab");
+  system("kill -SIGHUP 1");
+}
+void loop() 
+{
+
+}
+
+
