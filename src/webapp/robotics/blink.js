0f0c62eae5caffdf1e75109129dfc529eaddd227
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-05-19 22:40:33 -0300

    added microphone and photoResistor scripts

--- src/webapp/robotics/blink.js
+++ src/webapp/robotics/blink.js
@@ -1,21 +1,11 @@
+
 var five = require("johnny-five");
-
 var Galileo = require("galileo-io");
-
 var board = new five.Board({
-
   io: new Galileo()
-
 });
 
- 
-
- 
-
 board.on("ready", function() {
-
   var led = new five.Led(13);
-
   led.blink(2000);
-
 });
