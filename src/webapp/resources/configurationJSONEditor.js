3e86f817e0121bb8aac4c2b9e824180455674c32
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-05-22 17:08:39 -0300

    polished jsonEditor.js

--- src/webapp/resources/configurationJSONEditor.js
+++ src/webapp/resources/configurationJSONEditor.js
@@ -1,24 +1,29 @@
 var jsonfile = require('jsonfile');
 
-var file = 'configuration.json';
-var obj = {name: 'Tomas', lastName: 'Fernández'};
-var edit ='TomasEditted';
+module.exports = {
 
-jsonfile.writeFile(file, obj, function (err) {
-    if(err === null) console.log("It was written correctly");
-    else console.error(err);
+    setConfiguration: function(file, luminosityParam, decibelsParam, startTimeParam, endTimeParam){
 
-    var currentPackage = require('./configuration.json');
-    var currentStringPackage = JSON.stringify(currentPackage); // si esta línea no está y se imprime un string al lado como abajo, aparece que son solamente 2 objetos
-    console.log('The contents are: ' + currentStringPackage);
-    console.log('or: ');
-    console.log('Name: ' + JSON.stringify(currentPackage.name));
-    console.log('Last Name: ' + JSON.stringify(currentPackage.lastName));
-});
-console.log('Reading file....');
+        var obj = {
+            luminosity: luminosityParam,
+            decibels: decibelsParam,
+            startTime: startTimeParam,
+            endTime: endTimeParam
 
-jsonfile.readFile(file, function(err, obj) {
-    console.log(obj.name + '1');
-    console.log(obj.lastName);
-  console.dir(obj);
-});
+        };
+        jsonfile.writeFile(file, obj, function(err){
+            if(err === null) console.log("it was written correctly");
+            else console.error(err);
+        });
+    },
+
+    readConfiguration: function(file){
+        jsonfile.readFile(file, function(err, obj){
+            if(err === null){
+                return obj;
+            }else{
+                console.error(err);
+            }
+        });
+    }
+};
