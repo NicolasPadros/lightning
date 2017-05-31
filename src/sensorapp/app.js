f7bbdc2d9826f12c0c7ecda43743d6ad25db1900
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-05-31 19:16:46 -0300

    added sensorapp

--- src/sensorapp/app.js
+++ src/sensorapp/app.js
@@ -1,46 +1,72 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const five = require('johnny-five');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-var app = express();
+let led = null;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/index.html')
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+five.Board().on('ready', function() {
+  console.log('Arduino is ready.');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // Initial state for the LED light
+  let state = {
+    red: 1, green: 1, blue: 1
+  };
+
+  // Map pins to digital inputs on the board
+  led = new five.Led.RGB({
+    pins: {
+      red: 6,
+      green: 3,
+      blue: 5
+    }
+  });
+
+  // Helper function to set the colors
+  let setStateColor = function(state) {
+    led.color({
+      red: state.red,
+      blue: state.blue,
+      green: state.green
+    });
+};
+
+  // Listen to the web socket connection
+  io.on('connection', function(client) {
+    client.on('join', function(handshake) {
+      console.log(handshake);
+    });
+
+    // Set initial state
+    setStateColor(state);
+
+  // Every time a 'rgb' event is sent, listen to it and grab its new values for each individual colour
+    client.on('rgb', function(data) {
+      state.red = data.color === 'red' ? data.value : state.red;
+      state.green = data.color === 'green' ? data.value : state.green;
+      state.blue = data.color === 'blue' ? data.value : state.blue;
+
+      // Set the new colors
+      setStateColor(state);
+
+      client.emit('rgb', data);
+      client.broadcast.emit('rgb', data);
+    });
+
+    // Turn on the RGB LED
+    led.on();
+  });
 });
 
-module.exports = app;
+const port = process.env.PORT || 3000;
+
+server.listen(port);
+console.log(`Server listening on http://localhost:${port}`);
