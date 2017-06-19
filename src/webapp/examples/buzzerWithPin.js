b9ae7f67606fdc2cf78bda2d9ba256d88fc131d6
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-06-19 13:47:50 -0300

    mmmmmm

--- src/webapp/examples/buzzerWithPin.js
+++ src/webapp/examples/buzzerWithPin.js
@@ -6,12 +6,17 @@ var board = new five.Board({
 
 board.on("ready", function() {
     var isButtonPressed;
+    var operate = false;
     var byte = 0;
     this.pinMode(12, this.MODES.INPUT);
     this.pinMode(9, this.MODES.OUTPUT);
 
     setInterval(function() {
         board.digitalRead(12, function(data){ isButtonPressed = data; });
-        if(isButtonPressed === 0) board.digitalWrite(9, (byte ^= 1));
-    },  10000);
+        if(isButtonPressed === 0){
+                operate = !operate;
+                console.log("button was pressed");
+        }
+        if(operate === true) board.digitalWrite(9, (byte ^= 1));
+    },  2000);
 });
