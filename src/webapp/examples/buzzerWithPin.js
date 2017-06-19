d57480b33073242a2d3f10ca13326fbf2873f194
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-06-19 13:33:03 -0300

    added button to buzzer with pins

--- src/webapp/examples/buzzerWithPin.js
+++ src/webapp/examples/buzzerWithPin.js
@@ -1,11 +1,17 @@
+var five = require("johnny-five");
 var Galileo = require("galileo-io");
-var board = new Galileo();
+var board = new five.Board({
+  io: new Galileo()
+});
 
 board.on("ready", function() {
-  var byte = 0;
-  this.pinMode(9, this.MODES.OUTPUT);
+    var isButtonPressed;
+    var byte = 0;
+    this.pinMode(12, this.MODES.INPUT);
+    this.pinMode(9, this.MODES.OUTPUT);
 
-  setInterval(function() {
-    board.digitalWrite(9, (byte ^= 1));
-  }, 500);
+    setInterval(function() {
+        board.digitalRead(12, function(data){ isButtonPressed = data; });
+        if(isButtonPressed === 0) board.digitalWrite(9, (byte ^= 1));
+    },  10000);
 });
