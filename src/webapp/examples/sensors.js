9ef91acaf759838dd273c1367d76b69662cdf5b8
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-06-19 14:23:50 -0300

    updated sensors

--- src/webapp/examples/sensors.js
+++ src/webapp/examples/sensors.js
@@ -27,4 +27,24 @@ board.on('ready', function() {
     mic.on("data", function() {
         console.log("Mic data: " + this.value);
     });
+
+    var anode = new five.Led.RGB({
+      pins: {
+        red: 6,
+        green: 5,
+        blue: 3
+      },
+      isAnode: true
+    });
+
+    // Add led to REPL (optional)
+    this.repl.inject({
+      anode: anode
+    });
+
+    // Turn it on and set the initial color
+    anode.on();
+    anode.color("#FF0000");
+
+    anode.blink(1000);
 });
